876b4fb93e55efc4dfa496a5e6fa1a7271d8c0fc
kei_c <kei_c@192.168.40.156>
2024-05-22 13:36:30 +0900

    計算ロジックのメソッド化・クラスの切り分け

---
+++ src/Logic.java
@@ -0,0 +1,43 @@
+// 計算ロジック
+
+import java.util.Scanner;
+
+public class Logic {
+
+	public static void sankaku(Scanner scanner) {
+		// 三角形面積計算機			
+		System.out.println("底辺を入力してください");
+		int a = scanner.nextInt();
+		
+		System.out.println("高さを入力してください");
+		int b = scanner.nextInt();
+		
+		int c = (a * b) / 2;
+		
+		System.out.println("底辺 " + a + " 高さ " + b + " の三角形の面積は " + c);
+	}
+	
+	public static void shikaku(Scanner scanner) {
+		// 四角形面積計算機
+		System.out.println("縦幅を入力してください");
+		int a = scanner.nextInt();
+		
+		System.out.println("横幅を入力してください");		
+		int b = scanner.nextInt();
+		
+		int c = a * b;
+		
+		System.out.println("縦幅 " + a + " 横幅 " + b + " の四角形の面積は " + c);
+	}
+	
+	public static void maru(Scanner scanner) {
+		// 円面積計算機
+		System.out.println("半径を入力してください");
+		int a = scanner.nextInt();
+				
+		int c = a * a * 3;
+		
+		System.out.println("半径 " + a  + " の円の面積は " + c);
+	}
+
+}
--- src/Sumple01.java
+++ src/Sumple01.java
@@ -6,62 +6,35 @@ public class Sumple01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+
 		
-		// 面積計算に使う変数達
-		int a;
-		int b;
-		int c;
-		int mode;
+		Scanner scanner = new Scanner(System.in);
 		
 		// 計算モードの選択
 		System.out.println("どの計算を行いますか？");
 		System.out.println("1：三角形の面積");
 		System.out.println("2：四角形の面積");
 		System.out.println("3：円の面積");
-		mode = scanner.nextInt();
+		int mode = scanner.nextInt();
 		
 		switch(mode) {
 		case 1:
-			// 三角形面積計算機
-			System.out.println("底辺を入力してください");
-			a = scanner.nextInt();
-			
-			System.out.println("高さを入力してください");
-			b = scanner.nextInt();
-			
-			c = (a * b) / 2;
-			
-			System.out.println("底辺 " + a + " 高さ " + b + " の三角形の面積は " + c);
+			Logic.sankaku(scanner);
 			break;
 			
 		case 2:
-			// 四角形面積計算機
-			System.out.println("縦幅を入力してください");
-			a = scanner.nextInt();
-			
-			System.out.println("横幅を入力してください");		
-			b = scanner.nextInt();
-			
-			c = a * b;
-			
-			System.out.println("縦幅 " + a + " 横幅 " + b + " の四角形の面積は " + c);
+			Logic.shikaku(scanner);
 			break;
 			
 		case 3:
-			// 円面積計算機
-			System.out.println("半径を入力してください");
-			a = scanner.nextInt();
-					
-			c = a * a * 3;
-			
-			System.out.println("半径 " + a  + " の円の面積は " + c);
+			Logic.maru(scanner);
 			break;
 		
 		default:
 			System.out.println("エラー！1～3の中から入力してください！");
 		}
-
+		
+		scanner.close();
 	}
 
 }
