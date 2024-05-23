8960f81c1f36a15dad6d479b4694fc16f7b3f0f3
kei_c <kei_c@192.168.40.156>
2024-05-23 11:38:06 +0900

    feat refactor:#4 入力用メソッドgetInputを定義

--- src/Logic.java
+++ src/Logic.java
@@ -4,43 +4,44 @@ import java.util.Scanner;
 
 public class Logic {
 
-	public static void sankaku(Scanner scanner) {
-		// 三角形面積計算機			
-		System.out.println("底辺を入力してください");
-		int a = scanner.nextInt();
+	public static void triangleArea(Scanner scanner) {
+		// 三角形面積計算機
+		int base = getInput(scanner, "底辺を入力してください");
+		int height = getInput(scanner, "高さを入力してください");
 		
-		System.out.println("高さを入力してください");
-		int b = scanner.nextInt();
-		
-		int c = (a * b) / 2;
+		double area = (double)(base * height) / 2;
 		
 		System.out.println("------------------------------------------------------");
-		System.out.println("底辺 " + a + " 高さ " + b + " の三角形の面積は " + c);
+		System.out.println("底辺 " + base + " 高さ " + height + " の三角形の面積は " + area);
 	}
 	
-	public static void shikaku(Scanner scanner) {
+	public static void squareArea(Scanner scanner) {
 		// 四角形面積計算機
-		System.out.println("縦幅を入力してください");
-		int a = scanner.nextInt();
+		int base = getInput(scanner, "底辺を入力してください");
+		int height = getInput(scanner, "高さを入力してください");
 		
-		System.out.println("横幅を入力してください");		
-		int b = scanner.nextInt();
-		
-		int c = a * b;
+		int area = base * height;
 		
 		System.out.println("------------------------------------------------------");
-		System.out.println("縦幅 " + a + " 横幅 " + b + " の四角形の面積は " + c);
+		System.out.println("縦幅 " + base + " 横幅 " + height + " の四角形の面積は " + area);
 	}
 	
-	public static void maru(Scanner scanner) {
+	public static void circleArea(Scanner scanner) {
 		// 円面積計算機
-		System.out.println("半径を入力してください");
-		int a = scanner.nextInt();
+		int radius = getInput(scanner, "半径を入力してください");
 				
-		int c = a * a * 3;
+		double area = (double)(radius * radius) * 3.14;
 		
 		System.out.println("------------------------------------------------------");
-		System.out.println("半径 " + a  + " の円の面積は " + c);
+		System.out.println("半径 " + radius  + " の円の面積は " + area);
 	}
 
+	private static int getInput(Scanner scanner, String question) {
+		System.out.println(question);
+		while(!scanner.hasNextInt()) {
+			System.out.println("無効な入力です。整数を入力してください");
+			scanner.next();
+		}
+		return scanner.nextInt();
+	}
 }
